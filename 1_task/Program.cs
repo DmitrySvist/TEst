053d3aa750a01e9be42a49dd601fcaaf2a380a34
DmitrySvist <dimonsvistun@gmail.com>
2023-12-03 14:37:41 +0300

    программа для блок-схемы

--- 1_task/Program.cs
+++ 1_task/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+static int FindMin(int[] array)
+{
+    int min = array[0];
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+
+    return min;
+}
+
+static int FindMax(int[] array)
+{
+    int max = array[0];
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    } 
+
+    return max;
+}
+
+int[] array = { 4, 6, 1, 9, 2 };
+
+int min = FindMin(array);
+int max = FindMax(array);
+
+Console.WriteLine("Минимальный элемент: " + min);
+Console.WriteLine("Максимальный элемент: " + max);
