9b46ada08bc62631d3265eccdf42dc62c3f68d31
DmitrySvist <dimonsvistun@gmail.com>
2023-12-03 20:59:42 +0300

    Решение второй задачи

--- 2_task/Program.cs
+++ 2_task/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] FilterStrings(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] newArray = new string[count];
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[index] = array[i];
+            index++;
+        }
+    }
+
+    return newArray;
+}
+
+void PrintArray (string[] newArray)
+{
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        System.Console.Write(newArray[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+Console.WriteLine("Введите элементы массива, разделяя их запятой:");
+string[] inputArray = Console.ReadLine().Split(',');
+string[] newArray = FilterStrings(inputArray);
+PrintArray(newArray);
+
+
+
+
